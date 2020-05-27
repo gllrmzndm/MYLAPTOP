b16ba73c38d235aa327c328f38bd111f2db80383
Guillermo <guillermozaandam@gmail.com>
2020-05-27 03:51:09 +0200

    ...

--- Pop_OS_Installation.sh
+++ Pop_OS_Installation.sh
@@ -59,6 +59,9 @@ sudo apt-get install -y gnome-firmware
 sudo apt-get install -y clamav clamtk clamdscan
 sudo apt-get install -y remmina
 sudo apt-get install -y transmission
+
 sudo apt-get install -y vim
+yes 3 | sudo update-alternatives --config editor
+
 sudo apt install ubuntu-restricted-extras
 sudo apt-get install -y qemu qemu-kvm bridge-utils virt-manager libguestfs-tools gnome-boxes
