6049e6204a98d042783807f4af47d380c0672f05
Guillermo Zaandam <guillermozaandam@hotmail.com>
2022-09-04 21:31:25 +0200

    readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# MY LAPTOP
+
+#### Repository for semi-automatically installing my devices.
+
+[![macbook.jpg](https://i.postimg.cc/SxFW7mgp/macbook.jpg)](https://postimg.cc/2VGbCNpH)
---
+++ windows_install.ps1
@@ -0,0 +1,51 @@
+choco install -y Chocolatey v1.1.0
+choco install -y autohotkey
+choco install -y autohotkey.install
+choco install -y chocolatey
+choco install -y chocolatey-compatibility.extension
+choco install -y chocolatey-core.extension
+choco install -y chocolatey-windowsupdate.extension
+choco install -y dotnet-6.0-desktopruntime
+choco install -y dotnet-desktopruntime
+choco install -y DotNet4.5.2
+choco install -y Everything
+choco install -y figma
+choco install -y Firefox
+choco install -y flow-launcher
+choco install -y gh
+choco install -y git
+choco install -y git.install
+choco install -y GoogleChrome
+choco install -y KB2919355
+choco install -y KB2919442
+choco install -y KB2999226
+choco install -y KB3033929
+choco install -y KB3035131
+choco install -y KB3063858
+choco install -y mobaxterm
+choco install -y nmap
+choco install -y nodejs
+choco install -y nodejs.install
+choco install -y notepadplusplus
+choco install -y notepadplusplus.install
+choco install -y powerautomatedesktop
+choco install -y powertoys
+choco install -y python3
+choco install -y rpi-imager
+choco install -y rsync
+choco install -y rufus
+choco install -y ueli
+choco install -y vagrant
+choco install -y vcredist140
+choco install -y vcredist2015
+choco install -y virtualbox
+choco install -y vscode
+choco install -y vscode.install
+choco install -y wincompose
+choco install -y wincompose.install
+choco install -y 44 packages installed.
+choco install -y
+choco install -y Did you know Pro / Business automatically syncs with Programs and
+choco install -y  Features? Learn more about Package Synchronizer at
+choco install -y  https://chocolatey.org/compare
+choco install -y sharex
